25d1fa0106c8466d77b9d28e076d4564d69a84d7
derekuang <kuangyaode@gmail.com>
2024-10-25 16:02:33 +0000

    Commit Problem 2.

---
+++ Project 2: JavaScript Calisthenics/cs142-template-processor.js
@@ -0,0 +1,24 @@
+"use strict";
+
+class Cs142TemplateProcessor {
+	constructor(template) {
+		this.template = template;
+	}
+
+	fillIn(dictionary) {
+		let str = this.template;
+
+		for (const key in dictionary) {
+			if (!Object.hasOwn(dictionary, key)) {
+				continue;
+			}
+			str = str.replace("{{" + key + "}}", dictionary[key]);
+		}
+		
+		str = str.replaceAll(/{{\w+}}/g, "");
+		
+		return str;
+	}
+}
+
+window.Cs142TemplateProcessor = Cs142TemplateProcessor;
