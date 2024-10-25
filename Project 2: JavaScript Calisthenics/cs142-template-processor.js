2f62a6c7c29b1b60343a4e587c160e3935b177f1
derekuang <kuangyaode@gmail.com>
2024-10-25 17:32:38 +0000

    Change indent.

--- Project 2: JavaScript Calisthenics/cs142-template-processor.js
+++ Project 2: JavaScript Calisthenics/cs142-template-processor.js
@@ -1,24 +1,24 @@
 "use strict";
 
 class Cs142TemplateProcessor {
-	constructor(template) {
-		this.template = template;
-	}
+    constructor(template) {
+        this.template = template;
+    }
 
-	fillIn(dictionary) {
-		let str = this.template;
+    fillIn(dictionary) {
+        let str = this.template;
 
-		for (const key in dictionary) {
-			if (!Object.hasOwn(dictionary, key)) {
-				continue;
-			}
-			str = str.replace("{{" + key + "}}", dictionary[key]);
-		}
-		
-		str = str.replaceAll(/{{\w+}}/g, "");
-		
-		return str;
-	}
+        for (const key in dictionary) {
+            if (!Object.hasOwn(dictionary, key)) {
+                continue;
+            }
+            str = str.replace("{{" + key + "}}", dictionary[key]);
+        }
+        
+        str = str.replaceAll(/{{\w+}}/g, "");
+        
+        return str;
+    }
 }
 
 window.Cs142TemplateProcessor = Cs142TemplateProcessor;
