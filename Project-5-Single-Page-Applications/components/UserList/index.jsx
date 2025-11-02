36d558b92c4a49d2d28d297688b33347957c8622
derekuang <kuangyaode@gmail.com>
2025-11-02 17:29:15 +0800

    Implement dynamic user list with model data
    - Replace static example content with dynamic user list from
      window.cs142models.userListModel()
    - Remove instructional text and example list items
    - Add state management to fetch and display user data
    - Clean up unused imports by removing Typography component
    - Fix HTML formatting and indentation in photo-share.html
    - Remove welcome message route from main application

--- Project-5-Single-Page-Applications/components/UserList/index.jsx
+++ Project-5-Single-Page-Applications/components/UserList/index.jsx
@@ -1,11 +1,5 @@
 import React from "react";
-import {
-  Divider,
-  List,
-  ListItem,
-  ListItemText,
-  Typography,
-} from "@mui/material";
+import { Divider, List, ListItem, ListItemText } from "@mui/material";
 
 import "./styles.css";
 
@@ -15,34 +9,28 @@ import "./styles.css";
 class UserList extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      users: window.cs142models.userListModel(),
+    };
   }
 
   render() {
     return (
       <div>
-        <Typography variant="body1">
-          This is the user list, which takes up 3/12 of the window. You might
-          choose to use <a href="https://mui.com/components/lists/">Lists</a>{" "}
-          and <a href="https://mui.com/components/dividers/">Dividers</a> to
-          display your users like so:
-        </Typography>
         <List component="nav">
-          <ListItem>
-            <ListItemText primary="Item #1" />
-          </ListItem>
-          <Divider />
-          <ListItem>
-            <ListItemText primary="Item #2" />
-          </ListItem>
-          <Divider />
-          <ListItem>
-            <ListItemText primary="Item #3" />
-          </ListItem>
-          <Divider />
+          {this.state.users.map((user) => {
+            return (
+              <>
+                <ListItem key={user._id}>
+                  <ListItemText
+                    primary={user.first_name + " " + user.last_name}
+                  />
+                </ListItem>
+                <Divider />
+              </>
+            );
+          })}
         </List>
-        <Typography variant="body1">
-          The model comes in from window.cs142models.userListModel()
-        </Typography>
       </div>
     );
   }
